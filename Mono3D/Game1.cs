e28cc92c3ca91a4c61caf30dbf3daff82d9e9d76
banestyrelsen <valdemarjohanse@gmail.com>
2025-10-15 17:46:45 +0200

    Terrain basics

--- Mono3D/Game1.cs
+++ Mono3D/Game1.cs
@@ -15,6 +15,9 @@ public class Game1 : Game
     private Matrix _viewMatrix;
     private Matrix _projectionMatrix;
     private float _angle = 0f;
+    private int _terrainWidth = 4;
+    private int _terrainHeight = 3;
+    private float[,] _heightData;
 
     public Game1()
     {
@@ -36,44 +39,80 @@ public class Game1 : Game
 
     private void SetUpVertices()
     {
-        _vertices = new VertexPositionColor[5]
+        _vertices = new VertexPositionColor[_terrainWidth * _terrainHeight];
+        for (int x = 0; x < _terrainWidth; x++)
         {
-            new VertexPositionColor() {Position = new Vector3(0f, 0f, 0f), Color = Color.White},
-            new VertexPositionColor() {Position = new Vector3(5f, 0f, 0f), Color = Color.White},
-            new VertexPositionColor() {Position = new Vector3(10f, 0f, 0f), Color = Color.White},
-            new VertexPositionColor() {Position = new Vector3(5f, 0f, -5f), Color = Color.White},
-            new VertexPositionColor() {Position = new Vector3(10f, 0f, -5f), Color = Color.White}
-        };
+            for (int y = 0; y < _terrainHeight; y++)
+            {
+                _vertices[x + y * _terrainWidth].Position = new Vector3(x, _heightData[x,y], -y);
+                _vertices[x + y * _terrainWidth].Color = Color.White;
+            }
+        }
     }
 
     private void SetUpIndices()
     {
-        _indices = new int[6];
+        _indices = new int[(_terrainWidth - 1) * (_terrainHeight - 1) * 6];
+        int counter = 0;
+        for (int y = 0; y < _terrainHeight - 1; y++)
+        {
+            for (int x = 0; x < _terrainWidth - 1; x++)
+            {
+                int lowerLeft = x + y * _terrainWidth;
+                int lowerRight = (x + 1) + y * _terrainWidth;
+                int topLeft = x + (y + 1) * _terrainWidth;
+                int topRight = (x + 1) + (y + 1) * _terrainWidth;
 
-        _indices[0] = 3;
-        _indices[1] = 1;
-        _indices[2] = 0;
-        _indices[3] = 4;
-        _indices[4] = 2;
-        _indices[5] = 1;
+                _indices[counter++] = topLeft;
+                _indices[counter++] = lowerRight;
+                _indices[counter++] = lowerLeft;
+                
+                _indices[counter++] = topLeft;
+                _indices[counter++] = topRight;
+                _indices[counter++] = lowerRight;
+            }
+        }
     }    
     
     private void SetUpCamera()
     {
-        _viewMatrix = Matrix.CreateLookAt(new Vector3(0, 50, 0), new Vector3(0, 0, 0), new Vector3(0, 0, -1));
+        _viewMatrix = Matrix.CreateLookAt(new Vector3(0, 10, 0), new Vector3(0, 0, 0), new Vector3(0, 0, -1));
         _projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, _device.Viewport.AspectRatio, 1.0f, 300.0f);
     }    
+    
+    private void LoadHeightData()
+    {
+        _heightData = new float[4, 3];
+        _heightData[0, 0] = 0;
+        _heightData[1, 0] = 0;
+        _heightData[2, 0] = 0;
+        _heightData[3, 0] = 0;
+
+        _heightData[0, 1] = 0.5f;
+        _heightData[1, 1] = 0;
+        _heightData[2, 1] = -1.0f;
+        _heightData[3, 1] = 0.2f;
+
+        _heightData[0, 2] = 1.0f;
+        _heightData[1, 2] = 1.2f;
+        _heightData[2, 2] = 0.8f;
+        _heightData[3, 2] = 0;
+    }
     
     protected override void LoadContent()
     {
         _device = _graphics.GraphicsDevice;
 
         _effect = Content.Load<Effect>("effects");        
+        SetUpCamera();
+
+        LoadHeightData();
         SetUpVertices();
         SetUpIndices();
-        SetUpCamera();
     }
 
+
+    
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||

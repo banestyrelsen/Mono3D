26244148e2012c9013ee51107d2875a30db3c1b0
banestyrelsen <valdemarjohanse@gmail.com>
2025-10-22 20:38:51 +0200

    311

--- Mono3DPart3/Content/v_effects.fx
+++ Mono3DPart3/Content/v_effects.fx
@@ -1,5 +1,9 @@
 ﻿float4x4 xWorldViewProjection;
 Texture xTexture;
+float4x4 xWorld;
+float3 xLightPos;
+float xLightPower;
+float xAmbient;
 
 sampler TextureSampler = sampler_state { texture = <xTexture> ; magfilter = LINEAR; minfilter = LINEAR; mipfilter=LINEAR; AddressU = mirror; AddressV = mirror;};
 
@@ -7,6 +11,8 @@ struct VertexToPixel
 {
     float4 Position     : POSITION;
     float2 TexCoords    : TEXCOORD0;
+    float3 Normal        : TEXCOORD1;
+    float3 Position3D    : TEXCOORD2;
 };
 
 struct PixelToFrame
@@ -14,21 +20,36 @@ struct PixelToFrame
     float4 Color        : COLOR0;
 };
 
-VertexToPixel SimplestVertexShader( float4 inPos : POSITION, float2 inTexCoords : TEXCOORD0)
+VertexToPixel SimplestVertexShader( float4 inPos : POSITION0, float3 inNormal: NORMAL0, float2 inTexCoords : TEXCOORD0)
 {
     VertexToPixel Output = (VertexToPixel)0;
 
     Output.Position =mul(inPos, xWorldViewProjection);
     Output.TexCoords = inTexCoords;
+    Output.Normal = normalize(mul(inNormal, (float3x3)xWorld));
+    Output.Position3D = mul(inPos, xWorld);
 
     return Output;
 }
 
+float DotProduct(float3 lightPos, float3 pos3D, float3 normal)
+{
+    float3 lightDir = normalize(pos3D - lightPos);
+    return dot(-lightDir, normal);
+}
+
 PixelToFrame OurFirstPixelShader(VertexToPixel PSIn)
 {
-    PixelToFrame Output = (PixelToFrame)0;
+    PixelToFrame Output = (PixelToFrame)0;    
 
-    Output.Color = tex2D(TextureSampler, PSIn.TexCoords);
+    float diffuseLightingFactor = DotProduct(xLightPos, PSIn.Position3D, PSIn.Normal);
+    diffuseLightingFactor = saturate(diffuseLightingFactor);
+    diffuseLightingFactor *= xLightPower;
+
+    PSIn.TexCoords.y--;
+    float4 baseColor = tex2D(TextureSampler, PSIn.TexCoords);
+    Output.Color = baseColor*(diffuseLightingFactor + xAmbient);
+
     return Output;
 }
 
--- Mono3DPart3/Game1.cs
+++ Mono3DPart3/Game1.cs
@@ -10,16 +10,19 @@ public class Game1 : Game
     {
         private Vector3 position;
         private Vector2 texCoord;
-
-        public MyOwnVertexFormat(Vector3 position, Vector2 texCoord)
+        private Vector3 normal;
+        
+        public MyOwnVertexFormat(Vector3 position, Vector2 texCoord, Vector3 normal)
         {
             this.position = position;
             this.texCoord = texCoord;
+            this.normal = normal;            
         }
         public readonly static VertexDeclaration VertexDeclaration = new VertexDeclaration
         (
             new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
-            new VertexElement(sizeof(float) * 3, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0)
+            new VertexElement(sizeof(float) * 3, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+            new VertexElement(sizeof(float) * (3+2), VertexElementFormat.Vector3, VertexElementUsage.Normal, 0)
         );
     }
 
@@ -34,6 +37,11 @@ public class Game1 : Game
     Texture2D streetTexture;
     Model carModel;
     Texture2D[] carTextures;
+    Model lamppostModel;
+    Texture2D[] lamppostTextures;
+    Vector3 lightPos;
+    float lightPower;
+    float ambientPower;
     
     public Game1()
     {
@@ -72,31 +80,37 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         device = GraphicsDevice;
-
-
         effect = Content.Load<Effect>("v_effects");
-        streetTexture = Content.Load<Texture2D> ("streettexture");
-        SetUpVertices();
         SetUpCamera();
+        streetTexture = Content.Load<Texture2D> ("texturemap");
         carModel = LoadModel("ferrari", out carTextures);
+        lamppostModel = LoadModel("Lampa", out lamppostTextures);
+        lamppostTextures[0] = streetTexture;
+        SetUpVertices();
     }
 
     private void SetUpVertices()
     {
-        MyOwnVertexFormat[] vertices = new MyOwnVertexFormat[12];
+        MyOwnVertexFormat[] vertices = new MyOwnVertexFormat[18];
 
-        vertices[0] = new MyOwnVertexFormat(new Vector3(-20, 0, 10), new Vector2(-0.25f, 25.0f));
-        vertices[1] = new MyOwnVertexFormat(new Vector3(-20, 0, -100), new Vector2(-0.25f, 0.0f));
-        vertices[2] = new MyOwnVertexFormat(new Vector3(2, 0, 10), new Vector2(0.25f, 25.0f));
-        vertices[3] = new MyOwnVertexFormat(new Vector3(2, 0, -100), new Vector2(0.25f, 0.0f));
-        vertices[4] = new MyOwnVertexFormat(new Vector3(2, 1, 10), new Vector2(0.375f, 25.0f));
-        vertices[5] = new MyOwnVertexFormat(new Vector3(2, 1, -100), new Vector2(0.375f, 0.0f));
-        vertices[6] = new MyOwnVertexFormat(new Vector3(3, 1, 10), new Vector2(0.5f, 25.0f));
-        vertices[7] = new MyOwnVertexFormat(new Vector3(3, 1, -100), new Vector2(0.5f, 0.0f));
-        vertices[8] = new MyOwnVertexFormat(new Vector3(13, 1, 10), new Vector2(0.75f, 25.0f));
-        vertices[9] = new MyOwnVertexFormat(new Vector3(13, 1, -100), new Vector2(0.75f, 0.0f));
-        vertices[10] = new MyOwnVertexFormat(new Vector3(13, 21, 10), new Vector2(1.25f, 25.0f));
-        vertices[11] = new MyOwnVertexFormat(new Vector3(13, 21, -100), new Vector2(1.25f, 0.0f));
+        vertices[0] = new MyOwnVertexFormat(new Vector3(-20, 0, 10), new Vector2(-0.25f, 25.0f), new Vector3(0, 1, 0));
+        vertices[1] = new MyOwnVertexFormat(new Vector3(-20, 0, -100), new Vector2(-0.25f, 0.0f), new Vector3(0, 1, 0));
+        vertices[2] = new MyOwnVertexFormat(new Vector3(2, 0, 10), new Vector2(0.25f, 25.0f), new Vector3(0, 1, 0));
+        vertices[3] = new MyOwnVertexFormat(new Vector3(2, 0, -100), new Vector2(0.25f, 0.0f), new Vector3(0, 1, 0));
+        vertices[4] = new MyOwnVertexFormat(new Vector3(2, 0, 10), new Vector2(0.25f, 25.0f), new Vector3(-1, 0, 0));
+        vertices[5] = new MyOwnVertexFormat(new Vector3(2, 0, -100), new Vector2(0.25f, 0.0f), new Vector3(-1, 0, 0));
+        vertices[6] = new MyOwnVertexFormat(new Vector3(2, 1, 10), new Vector2(0.375f, 25.0f), new Vector3(-1, 0, 0));
+        vertices[7] = new MyOwnVertexFormat(new Vector3(2, 1, -100), new Vector2(0.375f, 0.0f), new Vector3(-1, 0, 0));
+        vertices[8] = new MyOwnVertexFormat(new Vector3(2, 1, 10), new Vector2(0.375f, 25.0f), new Vector3(0, 1, 0));
+        vertices[9] = new MyOwnVertexFormat(new Vector3(2, 1, -100), new Vector2(0.375f, 0.0f), new Vector3(0, 1, 0));
+        vertices[10] = new MyOwnVertexFormat(new Vector3(3, 1, 10), new Vector2(0.5f, 25.0f), new Vector3(0, 1, 0));
+        vertices[11] = new MyOwnVertexFormat(new Vector3(3, 1, -100), new Vector2(0.5f, 0.0f), new Vector3(0, 1, 0));
+        vertices[12] = new MyOwnVertexFormat(new Vector3(13, 1, 10), new Vector2(0.75f, 25.0f), new Vector3(0, 1, 0));
+        vertices[13] = new MyOwnVertexFormat(new Vector3(13, 1, -100), new Vector2(0.75f, 0.0f), new Vector3(0, 1, 0));
+        vertices[14] = new MyOwnVertexFormat(new Vector3(13, 1, 10), new Vector2(0.75f, 25.0f), new Vector3(-1, 0, 0));
+        vertices[15] = new MyOwnVertexFormat(new Vector3(13, 1, -100), new Vector2(0.75f, 0.0f), new Vector3(-1, 0, 0));
+        vertices[16] = new MyOwnVertexFormat(new Vector3(13, 21, 10), new Vector2(1.25f, 25.0f), new Vector3(-1, 0, 0));
+        vertices[17] = new MyOwnVertexFormat(new Vector3(13, 21, -100), new Vector2(1.25f, 0.0f), new Vector3(-1, 0, 0));
 
         vertexBuffer = new VertexBuffer(device, MyOwnVertexFormat.VertexDeclaration, vertices.Length,BufferUsage.WriteOnly);
         vertexBuffer.SetData(vertices);
@@ -114,14 +128,56 @@ public class Game1 : Game
     {
     }
 
+    
+    private void UpdateLightData()
+    {
+        lightPos = new Vector3(-10, 4, -2);
+        lightPower = 1.0f;
+        ambientPower = 0.2f;
+    }
+    
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
             this.Exit();
 
+        UpdateLightData();
+        
         base.Update(gameTime);
     }
+    
+    protected override void Draw(GameTime gameTime)
+    {
+        device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
 
+        effect.CurrentTechnique = effect.Techniques["Simplest"];
+        effect.Parameters["xWorldViewProjection"].SetValue(Matrix.Identity * viewMatrix * projectionMatrix);
+        effect.Parameters["xTexture"].SetValue(streetTexture);
+
+        effect.Parameters["xWorld"].SetValue(Matrix.Identity);
+        effect.Parameters["xLightPos"].SetValue(lightPos);
+        effect.Parameters["xLightPower"].SetValue(lightPower);
+        effect.Parameters["xAmbient"].SetValue(ambientPower);
+        
+        foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+        {
+            pass.Apply();
+
+            device.SetVertexBuffer(vertexBuffer);
+            device.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 16);
+        }
+        Matrix car1Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateTranslation(-3, 0, -15);
+        DrawModel(carModel, carTextures, car1Matrix, "Simplest");
+ 
+        Matrix car2Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi * 5.0f / 8.0f) * Matrix.CreateTranslation(-28, 0, -1.9f);
+        DrawModel(carModel, carTextures, car2Matrix, "Simplest");
+        
+        Matrix lamp1Matric = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi * 5.0f / 8.0f) * Matrix.CreateTranslation(-15, 0, 1.9f);
+        DrawModel(lamppostModel, lamppostTextures, lamp1Matric, "Simplest");
+        
+        base.Draw(gameTime);
+    }
+    
     private void DrawModel(Model model, Texture2D[] textures, Matrix wMatrix, string technique)
     {            
         Matrix[] modelTransforms = new Matrix[model.Bones.Count];
@@ -132,35 +188,15 @@ public class Game1 : Game
             foreach (Effect currentEffect in mesh.Effects)
             {
                 Matrix worldMatrix = modelTransforms[mesh.ParentBone.Index] * wMatrix;
-                currentEffect.CurrentTechnique = currentEffect.Techniques[technique];                                            currentEffect.Parameters["xWorldViewProjection"].SetValue(worldMatrix* viewMatrix*projectionMatrix);
+                currentEffect.CurrentTechnique = currentEffect.Techniques[technique];                                            
+                currentEffect.Parameters["xWorldViewProjection"].SetValue(worldMatrix * viewMatrix * projectionMatrix);
                 currentEffect.Parameters["xTexture"].SetValue(textures[i++]);
+                currentEffect.Parameters["xWorld"].SetValue(worldMatrix);
+                currentEffect.Parameters["xLightPos"].SetValue(lightPos);
+                currentEffect.Parameters["xLightPower"].SetValue(lightPower);
+                currentEffect.Parameters["xAmbient"].SetValue(ambientPower);
             }
             mesh.Draw();
         }
-    }
-    
-    protected override void Draw(GameTime gameTime)
-    {
-        device.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.DarkSlateBlue, 1.0f, 0);
-
-        effect.CurrentTechnique = effect.Techniques["Simplest"];
-        effect.Parameters["xWorldViewProjection"].SetValue(Matrix.Identity * viewMatrix * projectionMatrix);
-        effect.Parameters["xTexture"].SetValue(streetTexture);
-
-        foreach (EffectPass pass in effect.CurrentTechnique.Passes)
-        {
-            pass.Apply();
-
-            device.SetVertexBuffer(vertexBuffer);
-            device.DrawPrimitives(PrimitiveType.TriangleStrip, 0, 10);
-            
-            Matrix car1Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateTranslation(-3, 0, -15);
-            DrawModel(carModel, carTextures, car1Matrix, "Simplest");
- 
-            Matrix car2Matrix = Matrix.CreateScale(4f) * Matrix.CreateRotationY(MathHelper.Pi * 5.0f / 8.0f) * Matrix.CreateTranslation(-28, 0, -1.9f);
-            DrawModel(carModel, carTextures, car2Matrix, "Simplest");
-        }
-
-        base.Draw(gameTime);
     }
 }
